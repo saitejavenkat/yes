36b5389ccf8ae0f3cc0197bdb1c389bec5d5599d
saitejavenkat <129094399+saitejavenkat@users.noreply.github.com>
2024-01-25 23:15:09 +0530

    Update try.js

--- try.js
+++ try.js
@@ -2,6 +2,8 @@ var a=document.querySelector(".apps");
 var c=document.querySelector(".close");
 var b=document.querySelector(".nav-bar");
 var e=document.querySelector(".X");
+var x=document.querySelector(".fir-col");
+var y=document.querySelector(".sec-col");
 function nav_click()
 {
     a.classList.add("move");
@@ -10,11 +12,21 @@ function nav_click()
 }
 
 function nav_close()
-{;
+{
     a.classList.remove("move");
     c.classList.remove("small-screen");
     e.classList.remove("bye");
     a.classList.add("bye");
+}
 
+function h()
+{ 
+    x.style.display="block";
+    y.classList.add("move");
+}
 
+function h1()
+{
+    x.style.display="none";
+    y.classList.remove("move");
 }
